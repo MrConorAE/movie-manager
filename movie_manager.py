8c312314491841ca7bdd1820444dc0ddede01eac
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2021-07-27 13:44:37 +1200

    Created the main menu and implemented the exit option.

--- movie_manager.py
+++ movie_manager.py
@@ -14,3 +14,32 @@ import sqlite3 as sql
 db = sql.connect('movies.db')
 # Initialise the cursor.
 c = db.cursor()
+
+# MAIN MENU
+# Create the main menu.
+while True:
+    mainMenuOptions = {'Search your library': 'search', 'Add a movie': 'add', 'Remove a movie': 'remove',
+                       'Update a movie': 'update', 'View your library': 'view', 'Exit': 'exit'}
+    mainMenuChoice = eg.buttonbox('Welcome to Movie Manager!\nPlease choose an option.',
+                                  'Movie Manager - Main Menu', list(mainMenuOptions.keys()))
+    if mainMenuChoice == 'search':
+        # Search for a movie.
+        pass
+    elif mainMenuChoice == 'add':
+        # Add a movie to the database.
+        pass
+    elif mainMenuChoice == 'remove':
+        # Remove a movie.
+        pass
+    elif mainMenuChoice == 'update':
+        # Update a movie.
+        pass
+    elif mainMenuChoice == 'view':
+        # View the movies in the database.
+        pass
+    elif mainMenuChoice == 'exit':
+        # Exit the program.
+        if (eg.buttonbox('Are you sure you want to exit?', 'Movie Manager - Exit', ('Yes', 'No'))) == 'Yes':
+            quit()
+        else:
+            pass
